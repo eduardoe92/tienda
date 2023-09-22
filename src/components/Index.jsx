e3043ffd742d7a34d28b0e55364a3dc9dff1c34f
Nico Garay <garay.nicolas002@gmail.com>
2023-09-22 20:35:03 -0300

    cambio menor en index

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -8,14 +8,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 const Index = () => {
     return (
-        <div>
-            <BrowserRouter>
+        <div>          
                 <NewCollection />
                 <DiscoveryCollection />
                 <PopularProducts />
                 <MostRated />
-                <NewShoes />
-            </BrowserRouter>
+                <NewShoes />          
         </div>
     )
 }
